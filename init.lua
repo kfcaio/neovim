dd5c503621cf2fce11d08b00d9630e67f13f0b7f
Kfcaio <caio96@tuta.io>
2024-09-22 12:38:39 -0300

    Adiciona config do Neovim

---
+++ init.lua
@@ -0,0 +1,348 @@
+local packer = require("packer")
+
+vim.cmd([[packadd packer.nvim]])
+
+packer.startup(function()
+  use("wbthomason/packer.nvim")
+  use("windwp/nvim-autopairs")
+  use("Mofiqul/dracula.nvim")
+  use("dense-analysis/ale")
+  use("airblade/vim-gitgutter")
+  use {
+    "nvim-lualine/lualine.nvim",
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use("tpope/vim-fugitive")
+  use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+	})
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-nvim-lsp")
+  use({
+  "kyazdani42/nvim-tree.lua",
+   requires = {
+    "kyazdani42/nvim-web-devicons",
+   },
+  })
+  use {
+    "rmagatti/goto-preview",
+    config = function()
+    require('goto-preview').setup {}
+    end
+  } 
+  use({
+    "junegunn/fzf.vim",
+    requires = { "junegunn/fzf", run = ":call fzf#install()" }
+ })
+ use {
+  'Exafunction/codeium.vim',
+  config = function ()
+    vim.keymap.set('i', '<leader>n', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    vim.keymap.set('i', '<leader>p', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+    vim.keymap.set('i', '<leader>x', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+  end
+}
+end)
+
+local set = vim.opt
+
+set.foldlevel = 99
+set.foldenable = false
+set.foldmethod = "indent"
+set.background = "dark"
+set.clipboard = "unnamedplus"
+set.completeopt = "noinsert,menuone,noselect"
+set.cursorline = true
+set.expandtab = true
+set.hidden = true
+set.inccommand = "split"
+set.mouse = "a"
+set.number = true
+set.shiftwidth = 2
+set.smarttab = true
+set.splitbelow = true
+set.splitright = true
+set.swapfile = false
+set.tabstop = 2
+set.termguicolors = true
+set.title = true
+set.ttimeoutlen = 0
+set.updatetime = 250
+set.wildmenu = true
+set.wrap = true
+
+vim.cmd([[
+  filetype plugin indent on
+  syntax on
+  filetype on
+  colorscheme dracula
+]])
+
+require('lualine').setup()
+
+require('goto-preview').setup {
+  width = 120; -- Width of the floating window
+  height = 15; -- Height of the floating window
+  border = {"↖", "─" ,"┐", "│", "┘", "─", "└", "│"}; -- Border characters of the floating window
+  default_mappings = false; -- Bind default mappings
+  debug = false; -- Print debug information
+  opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+  resizing_mappings = false; -- Binds arrow keys to resizing the floating window.
+  post_open_hook = nil; -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  post_close_hook = nil; -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
+  focus_on_open = true; -- Focus the floating window when opening it.
+  dismiss_on_move = false; -- Dismiss the floating window when moving the cursor.
+  force_close = true, -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
+  bufhidden = "wipe", -- the bufhidden option to set on the floating window. See :h bufhidden
+  stack_floating_preview_windows = true, -- Whether to nest floating windows
+  preview_window_title = { enable = true, position = "left" }, -- Whether to set the preview window title as the filename
+}
+
+local map = vim.api.nvim_set_keymap
+local kmap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = "`"
+
+vim.diagnostic.config({
+  float = { source = "always", border = border },
+  virtual_text = false,
+  signs = true,
+})
+
+require'nvim-tree'.setup({
+  view = {
+    adaptive_size = true,
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        item = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = false,
+      show = {
+        file = false,
+        folder = true,
+        folder_arrow = false,
+        git = true
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "", -- 
+          staged = "",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "",
+          deleted = "",
+          ignored = "◌",
+        },
+     },
+    },
+  }
+})
+
+vim.cmd([[ autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+
+vim.keymap.set('n', '<leader>f', '<cmd>:NvimTreeToggle<cr>')
+vim.keymap.set('n', '<leader>g', ':Ag<space>')
+vim.keymap.set("n", "<leader>d", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", {noremap=true})
+
+vim.g.ale_linters = {
+   python = {'flake8'}
+}
+
+vim.g.ale_set_balloons = 0
+
+vim.g.ale_python_flake8_options = '--max-line-length=79 --extend-ignore=E203'
+
+vim.g.python3_host_prog="/Users/jusbrasil/.pyenv/shims/python3"
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+local on_attach = function(_, bufnr)
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	-- Theme, colors and gui
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+	-- See `:help K` for why this keymap
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, { desc = "Format current buffer with LSP" })
+end
+
+local servers = {
+	pyright = {},
+	eslint = {
+		codeAction = {
+			disableRuleComment = {
+				enable = false,
+				location = "separateLine",
+			},
+			showDocumentation = {
+				enable = true,
+			},
+		},
+		codeActionOnSave = {
+			enable = false,
+			mode = "all",
+		},
+		format = false,
+		nodePath = "",
+		onIgnoredFiles = "off",
+		packageManager = "npm",
+		quiet = false,
+		rulesCustomizations = {},
+		run = "onType",
+		useESLintClass = false,
+		validate = "on",
+		workingDirectory = {
+			mode = "location",
+		},
+	},
+	bashls = {},
+	cssls = {},
+	html = {},
+	jsonls = {},
+	lemminx = {},
+	gopls = {},
+	lua_ls = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+}
+
+-- MASON.NVIM
+require("mason").setup()
+
+-- MASON-LSPCONFIG.NVIM
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup({
+	ensure_installed = vim.tbl_keys(servers),
+})
+
+mason_lspconfig.setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = servers[server_name],
+		})
+	end,
+})
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<C-t>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<C-k>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+}
+
+vim.opt.updatetime = 100
+
+
+
